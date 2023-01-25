d9a2f700d81fd1cd508aaaa6ad6794f48601d0d8
uduak <lorduduak@gmail.com>
2023-01-25 21:45:34 +0100

    foormhandler func

--- main.go
+++ main.go
@@ -21,7 +21,16 @@ func helloHander(w http.ResponseWriter, r *http.Request) {
 }
 
 func formHandler (w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm()
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
 }
 
 func main() {
