1cf7391921cacad12521a2f68bf7603583be8c8a
uduak <lorduduak@gmail.com>
2023-01-25 23:14:51 +0100

    fixed error on port number

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ func main() {
 
 	fmt.Printf("Starting server at port 8080\n")
 
-	if err := http.ListenAndServe(":8000", nil); err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
